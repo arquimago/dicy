4a123f7f055935f233b4fb19c3eab1c588c6ccf0
thales <thales@alvesnet.net>
2020-01-08 23:57:20 -0300

    adicionando animações aos cliques

--- app/src/main/java/net/alvesnet/dicy/MainActivity.kt
+++ app/src/main/java/net/alvesnet/dicy/MainActivity.kt
@@ -1,10 +1,19 @@
 package net.alvesnet.dicy
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.DecelerateInterpolator
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
+
 class MainActivity : AppCompatActivity() {
+
+    var d4Angle = 90f
+    var d6Angle = 90f
+    var d8Angle = 90f
+    var d10Angle = 90f
+    var d12Angle = 90f
+    var d20Angle = 90f
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,26 +23,50 @@ class MainActivity : AppCompatActivity() {
 
         d4.setOnClickListener{
             results.setText(Dice(sides = 4).roll())
+            d4.animate()
+                .rotation(d4Angle)
+                .setDuration(200).interpolator = DecelerateInterpolator()
+            d4Angle  += 90f
         }
 
         d6.setOnClickListener{
             results.setText(Dice(sides = 6).roll())
+            d6.animate()
+                .rotation(d6Angle)
+                .setDuration(200).interpolator = DecelerateInterpolator()
+            d6Angle  += 90f
         }
 
         d8.setOnClickListener{
             results.setText(Dice(sides = 8).roll())
+            d8.animate()
+                .rotation(d8Angle)
+                .setDuration(200).interpolator = DecelerateInterpolator()
+            d8Angle  += 90f
         }
 
         d10.setOnClickListener{
             results.setText(Dice(sides = 10).roll())
+            d10.animate()
+                .rotation(d10Angle)
+                .setDuration(200).interpolator = DecelerateInterpolator()
+            d10Angle  += 90f
         }
 
         d12.setOnClickListener{
             results.setText(Dice(sides = 12).roll())
+            d12.animate()
+                .rotation(d12Angle)
+                .setDuration(200).interpolator = DecelerateInterpolator()
+            d12Angle  += 90f
         }
 
         d20.setOnClickListener{
             results.setText(Dice(sides = 20).roll())
+            d20.animate()
+                .rotation(d20Angle)
+                .setDuration(200).interpolator = DecelerateInterpolator()
+            d20Angle  += 90f
         }
     }
 }

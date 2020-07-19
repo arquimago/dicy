6d3f77d4525ebae95d3bf16661befce679f7c026
Thales <thales@alvesnet.net>
2020-07-18 22:31:22 -0300

    função de rolagem na activity, animações modificadas

--- app/src/main/java/net/alvesnet/dicy/MainActivity.kt
+++ app/src/main/java/net/alvesnet/dicy/MainActivity.kt
@@ -1,19 +1,27 @@
 package net.alvesnet.dicy
 
 import android.os.Bundle
-import android.view.animation.DecelerateInterpolator
+import android.os.Handler
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.mikhaellopez.rxanimation.RxAnimation
+import com.mikhaellopez.rxanimation.press
+import com.mikhaellopez.rxanimation.rotation
+import com.mikhaellopez.rxanimation.text
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
 
-    var d4Angle = 90f
-    var d6Angle = 90f
-    var d8Angle = 90f
-    var d10Angle = 90f
-    var d12Angle = 90f
-    var d20Angle = 90f
+    companion object{
+        const val ANIMATION_DURATION = 400L
+    }
+    var d4Angle = 360f
+    var d6Angle = 360f
+    var d8Angle = 360f
+    var d10Angle = 360f
+    var d12Angle = 360f
+    var d20Angle = 360f
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,52 +29,71 @@ class MainActivity : AppCompatActivity() {
 
         results.isEnabled = false
 
-        d4.setOnClickListener{
-            results.setText(Dice(sides = 4).roll())
-            d4.animate()
-                .rotation(d4Angle)
-                .setDuration(200).interpolator = DecelerateInterpolator()
-            d4Angle  += 90f
+        d4.setOnClickListener {
+            roll(4, it)
         }
 
-        d6.setOnClickListener{
-            results.setText(Dice(sides = 6).roll())
-            d6.animate()
-                .rotation(d6Angle)
-                .setDuration(200).interpolator = DecelerateInterpolator()
-            d6Angle  += 90f
+        d6.setOnClickListener {
+            roll(6, it)
         }
 
-        d8.setOnClickListener{
-            results.setText(Dice(sides = 8).roll())
-            d8.animate()
-                .rotation(d8Angle)
-                .setDuration(200).interpolator = DecelerateInterpolator()
-            d8Angle  += 90f
+        d8.setOnClickListener {
+            roll(8,  it)
         }
 
-        d10.setOnClickListener{
-            results.setText(Dice(sides = 10).roll())
-            d10.animate()
-                .rotation(d10Angle)
-                .setDuration(200).interpolator = DecelerateInterpolator()
-            d10Angle  += 90f
+        d10.setOnClickListener {
+            roll(10,  it)
         }
 
-        d12.setOnClickListener{
-            results.setText(Dice(sides = 12).roll())
-            d12.animate()
-                .rotation(d12Angle)
-                .setDuration(200).interpolator = DecelerateInterpolator()
-            d12Angle  += 90f
+        d12.setOnClickListener {
+            roll(12,  it)
         }
 
-        d20.setOnClickListener{
-            results.setText(Dice(sides = 20).roll())
-            d20.animate()
-                .rotation(d20Angle)
-                .setDuration(200).interpolator = DecelerateInterpolator()
-            d20Angle  += 90f
+        d20.setOnClickListener {
+            roll(20,  it)
         }
+    }
+
+    private fun roll(sides: Int, view: View){
+
+        d4.isClickable = false
+        d6.isClickable = false
+        d8.isClickable = false
+        d10.isClickable = false
+        d12.isClickable = false
+        d20.isClickable = false
+
+        val angle = when(sides){
+            4 -> d4Angle
+            6 -> d6Angle
+            8 -> d8Angle
+            10 -> d10Angle
+            12 -> d12Angle
+            else -> d20Angle
+        }
+
+        RxAnimation.together(
+            view.press(duration = ANIMATION_DURATION),
+            results.text(Dice(sides = sides).roll(),200L),
+            view.rotation(angle, ANIMATION_DURATION)
+        ).subscribe()
+
+        when(sides){
+            4 -> d4Angle += 360f
+            6 -> d6Angle += 360f
+            8 -> d8Angle += 360f
+            10 -> d10Angle += 360f
+            12 -> d12Angle += 360f
+            else -> d20Angle += 360f
+        }
+
+        Handler().postDelayed({
+            d4.isClickable = true
+            d6.isClickable = true
+            d8.isClickable = true
+            d10.isClickable = true
+            d12.isClickable = true
+            d20.isClickable = true
+        }, ANIMATION_DURATION)
     }
 }
